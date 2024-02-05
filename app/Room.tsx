a893cb2450fbc7e42d8a4e108548a87cf4f1f9da
Pema Wangchuk <pemawangchug1986@gmail.com>
2024-02-06 00:20:29 +0600

    initial

---
+++ app/Room.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { ReactNode } from 'react';
+import { RoomProvider } from '../liveblocks.config';
+import { ClientSideSuspense } from '@liveblocks/react';
+import { LiveMap } from '@liveblocks/client';
+import Loader from '@/components/Loader';
+
+export function Room({ children }: { children: ReactNode }) {
+  return (
+    <RoomProvider
+      id='my-room'
+      initialPresence={{
+        cursor: null,
+        cursorColor: null,
+        editingText: null,
+      }}
+      initialStorage={{
+        canvasObjects: new LiveMap(),
+      }}
+    >
+      <ClientSideSuspense
+        fallback={
+          <div>
+            <Loader />
+          </div>
+        }
+      >
+        {() => children}
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
+}
---
+++ components/cursor/Cursor.tsx
@@ -0,0 +1,32 @@
+import CursorSVG from '@/public/assets/CursorSVG';
+import React from 'react';
+
+type Props = {
+  color: string;
+  x: number;
+  y: number;
+  message: string;
+};
+
+const Cursor = ({ color, x, y, message }: Props) => {
+  return (
+    <div
+      className='pointer-events-none absolute top-o left-0'
+      style={{ transform: `translate(${x}px) translate(${y}px)` }}
+    >
+      <CursorSVG color={color} />
+      {message && (
+        <div
+          className='absolute left-2 top-5 rounded-3xl px-4 py-2'
+          style={{ backgroundColor: color }}
+        >
+          <p className='text-white white-space-nowrap text-sm leading-relaxed'>
+            {message}
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Cursor;
